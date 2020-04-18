36a5af6f9d36b4435ab207a934156e07883c5f84
hitfishking <1063888400@qq.com>
2020-04-18 12:49:28 +0800

    add 2nd function

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,6 @@
 exports.handler = function(event, context, callback) {
-	const { name } = JSON.parse(event.body)
+	//body中是序列化的json，其中包括{"name":"james"}; 该ES6语法可以提取json中的部分字段。
+	const { name } = JSON.parse(event.body)   
 	if (name == 'james'){
 		callback(null, {
 			statusCode: 200,
---
+++ functions/test2.js
@@ -0,0 +1,17 @@
+//from https://www.raymondcamden.com/2019/01/08/adding-serverless-functions-to-your-netlify-static-site
+exports.handler = function(event, context, callback) {
+	
+	let data = {
+		name:'ray',
+		foo:[1,2,4,6,7,80],
+		time:Date.now()
+	};
+	
+	console.log('data is '+JSON.stringify(data));
+
+	callback(null, {
+		statusCode:200,
+		body:JSON.stringify(data)
+	});
+
+}
